a7b3615a5c77590a7ef3761b5f18cbc6aebaa86b
ritikasharma78 <bewithritika17@gmail.com>
2025-09-28 22:13:46 +0530

    home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
 export default function App() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-900 text-white">
-      <h1 className="text-4xl font-bold text-blue-400">Hello Tailwind!</h1>
-      <button className="mt-4 px-4 py-2 bg-blue-500 rounded-lg hover:bg-blue-600">
-        Click Me
-      </button>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-pink-200 text-white">
+      <h1 className="text-4xl font-bold text-pink-400">algorithms here:</h1>
+      
     </div>
   );
 }
