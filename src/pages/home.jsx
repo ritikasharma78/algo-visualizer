239cc1f769805e7ac4a57ca5f55d8caef2f2a86d
ManasPG <manasgoswamee@gmail.com>
2025-09-30 01:08:07 +0530

    Aurora

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
+import Aurora from "../components/ui/aurora/aurora";
 
 export default function HomePage() {
   return (
-   <div>xyz</div>
+    <>
+      <Aurora
+        colorStops={["#3A29FF", "#FF94B4", "#FF3232"]}
+        blend={0.5}
+        amplitude={1.0}
+        speed={0.5}
+      />
+    </>
   );
 }
